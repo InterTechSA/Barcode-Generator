f5ebda477e2126eaae65a6371f70c4e7cf8f38e6
Thabang Kubheka <104712625+InterTechSA@users.noreply.github.com>
2024-02-20 06:00:38 +0000

    Improve3d on the code

--- Barcode_Generator.py
+++ Barcode_Generator.py
@@ -1,15 +1,21 @@
 import barcode
 from barcode.writer import ImageWriter
-import os
 
-filename = input("Please provide name for your file: ")
+filename = input("Please provide a name for your file: ")
+number = input("Please enter the number you wish to print a barcode for: ")
 
-number = input("Please enter the number you wish to print barcode for: ")
+
+def is_eligible(num):
+    if len(num) == 13:
+        return True
+    else:
+        return False
+
+
+while not is_eligible(number):
+    print("Invalid input! The number must be exactly 13 digits.")
+    number = input("Please enter the number you wish to print a barcode for: ")
 
 ean = barcode.get_barcode_class('ean13')
 generated_code = ean(number, writer=ImageWriter())
-
 generated_code.save(filename)
-
-
-
